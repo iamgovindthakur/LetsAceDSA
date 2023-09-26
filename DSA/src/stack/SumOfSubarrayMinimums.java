58606dceb26ec58f29ab552c81d0ab4b3bc29569
Govind Kumar Thakur <iamgovindthakur@gmail.com>
2023-09-26 10:34:15 +0530

    907. Sum of Subarray Minimums approach 2 added

--- DSA/src/stack/SumOfSubarrayMinimums.java
+++ DSA/src/stack/SumOfSubarrayMinimums.java
@@ -4,6 +4,33 @@ import java.util.Stack;
 
 public class SumOfSubarrayMinimums {
 
+    static public int sumSubarrayMins2(int[] arr) {
+        long res = 0;
+        Stack<Integer> stack = new Stack<>();
+        long M = (long)1e9 + 7;
+        stack.push(-1);
+
+        for (int i2 = 0; i2 < arr.length+1; i2++){
+            int currVal = (i2<arr.length)? arr[i2] : 0;
+
+            while(stack.peek() !=-1 && currVal < arr[stack.peek()]){
+                int index = stack.pop();
+                int i1 = stack.peek();
+                int left = index - i1;
+                int right = i2 - index;
+                long add = (left * right * (long)arr[index]) % M;
+                res += add ;
+                res %= M;
+            }
+
+            stack.push(i2);
+
+        }
+
+        return (int)res;
+
+    }
+
     static public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         int[] left = new int[n];
@@ -45,6 +72,6 @@ public class SumOfSubarrayMinimums {
 
     public static void main(String[] args) {
         int [] arr = {11,81,94,43,3};
-        System.out.println(sumSubarrayMins(arr));
+        System.out.println(sumSubarrayMins2(arr));
     }
 }
